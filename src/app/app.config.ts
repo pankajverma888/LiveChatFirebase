15f0e6c668139a245e9399a8007d39b591a253cb
Pankaj Verma <letstalk@nuvemiq.com>
2025-07-07 23:51:38 +0530

    added code

---
+++ src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+ import {provideFirebaseApp, initializeApp} from '@angular/fire/app'
+import { firebaseConfig } from './firebase/firebase-config';
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+import { provideToastr } from 'ngx-toastr';
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideToastr(),
+    provideAnimations()
+  ]
+};
